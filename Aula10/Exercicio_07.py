db0d0cfcf33c0063b7741ca11574ee8a60253686
DheniMoura <91914066+DheniMoura@users.noreply.github.com>
2022-04-04 19:07:04 -0300

    Update Exercicio07.py

--- Aula10/Exercicio_07.py
+++ Aula10/Exercicio_07.py
@@ -1,9 +1,9 @@
-#cada um dos lados precisa menor que a soma dos outros dois lados.
+# cada um dos lados precisa menor que a soma dos outros dois lados.
 a = float(input('Lado 1 (cm): '))
 b = float(input('Lado 2 (cm): '))
 c = float(input('Lado 3 (cm): '))
 
 if a + b > c and a + c > b and b + c > a:
-    print('Triângulo feito!') 
+    print('Triângulo feito!')
 else:
-    print('Não forma um triângulo.') 
+    print('\033[31mNão forma\033[37m um triângulo.')
