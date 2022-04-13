cb26c4fd530baf889b727716b0ab20fda8f83f51
DheniMoura <91914066+DheniMoura@users.noreply.github.com>
2022-04-13 14:19:58 -0300

    Atualização

---
+++ Let's Code/11.04.22 1A.py
@@ -0,0 +1,14 @@
+'''
+Para um número *n* informado pelo usuário, use uma função
+que receba um valor *n* inteiro e imprima até a n-ésima linha
+1
+2 2
+3 3 3 
+......
+n n ... n '''
+
+n = int(input('Digite um número: '))
+
+for j in range (1, n+1):
+  for i in range(1, n+1):
+    print(f'{i} '* i)
---
+++ Let's Code/11.04.22 1B.py
@@ -0,0 +1,8 @@
+'''
+Para um número *n* informado pelo usuário, use uma função
+que receba um valor *n* inteiro e imprima até a n-ésima linha
+1
+1 2
+1 2 3 
+...... 
+1 2 3 ... n '''
